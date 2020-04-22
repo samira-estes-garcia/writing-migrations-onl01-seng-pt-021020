3fc632e0b0dbe2623f21106f65d845b676e35551
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-22 22:10:44 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,9 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration [5.1]
   
   def change
-    add_column do |c|
-      c.integer :grade 
-      c.string :birthdate 
+    add_column 
     end
   end
   
